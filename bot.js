4f9f11ef38e99d8259acda06318cd54a52025ef6
elkokoy <45782714+elkokoy@users.noreply.github.com>
2019-02-23 11:29:51 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -962,13 +962,36 @@ client.on('message',async msg => {
   setInterval(() => {
       var currentTime = new Date(),
 Year = currentTime.getFullYear(),
-Month = currentTime.getMonth() + 1,
+Month = currentTime.getMonth(),
 Dat = currentTime.getDate()
-      time.setName(`•RG members• : [ ${msg.guild.members.size} ]`);
+      time.setName(`•Member Count : [ ${msg.guild.members.size} ]•`);
  },1000);
   });
   }
 });
+
+
+
+client.on('message',async Epic => {
+  var prefix = "$" ;
+  if(Epic.content.startsWith(prefix + "vonline")) {
+  if(!Epic.guild.member(Epic.author).hasPermissions('MANAGE_CHANNELS')) return Epic.reply(':x: **I Dont Have Permissions**');
+  if(!Epic.guild.member(client.user).hasPermissions(['MANAGE_CHANNELS','MANAGE_ROLES_OR_PERMISSIONS'])) return Epic.reply(':x: **You Dont Have Permissions**');
+  Epic.guild.createChannel(`Voice Online : [ ${Epic.guild.members.filter(m => m.voiceChannel).size} ]` , 'voice').then(c => {
+    console.log(`Voice Online Is Activation In ${Epic.guild.name}`);
+    c.overwritePermissions(Epic.guild.id, {
+      CONNECT: false,
+      SPEAK: false
+    });
+    setInterval(() => {
+      c.setName(`Voice Online :  ${Epic.guild.members.filter(m => m.voiceChannel).size} .`)
+    },1000);
+  });
+  }
+});
+
+
+
 
 
 ////////////////////////////////////////////////////OWNER////////////////////////////////////////////////
